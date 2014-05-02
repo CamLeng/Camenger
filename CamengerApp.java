bd061ef811e61098feffa0768f43407bccffe1a2
Cameron Lengerich <cameron.lengerich@gmail.com>
2014-05-01 22:03:54 -0400

    Cleaned up JavaDoc comment

--- CamengerApp.java
+++ CamengerApp.java
@@ -1,13 +1,23 @@
 /**
 * @author Cameron Lengerich
 *
-* Camenger is a personal messenger. You can send text to and from * someone else that has this same app, provided you have a shared * folder of some sort. Camenger saves the serialized string object to * a file in your shared folder. The file can then be opened by * Camenger and displayed to the user in plain text.
-* 
-* Camenger is a very basic messenger, and is one of my first projects * on GitHub.
+* Camenger is a personal messenger. You can send text to and from
+* someone else that has this same app, provided you have a shared
+* folder of some sort. Camenger saves the serialized string object 
+* to a file in your shared folder. The file can then be opened by 
+*
+* Camenger and displayed to the user in plain text. Camenger is a 
+* very basic messenger, and is one of my first projects on GitHub.
 */
+
 public class CamengerApp {
+
+	/**
+	* Creates a frame object and sets it visible
+	*/
 	public static void main(String[] args) {
 		MyFrame frame = new MyFrame();
 		frame.setVisible(true);
 	}
+	
 }
